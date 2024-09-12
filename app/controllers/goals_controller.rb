1ccc54ce70906d6551795e0dc69154906ac08efb
Christopher Oak <coak@dualenroll.com>
2024-09-12 12:05:51 -0400

    Added new, create, and delete methods to controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,13 +8,36 @@ class GoalsController < ApplicationController
   end
 
   def new
+    @goal = Goal.new
+  end
 
+  def edit
+    @goal = Goal.find(params[:id])
   end
 
   def create
     @goal = Goal.new(params.require(:goal).permit(:name, :sport))
-    @goal.save
-    redirect_to @goal
+    if @goal.save
+      flash[:notice] = "Goal was created successfully."
+      redirect_to @goal
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    if @goal.update(params.require(:goal).permit(:name, :sport))
+      flash[:notice] = "Goal was updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to goals_path
   end
 
 end
