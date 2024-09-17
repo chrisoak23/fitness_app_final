509903f64834615e2e3117f8b1affdb6262486e4
Christopher Oak <coak@dualenroll.com>
2024-09-17 11:44:16 -0400

    Added password field to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_16_204804) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_17_153818) do
   create_table "goals", force: :cascade do |t|
     t.string "name"
     t.string "sport"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_16_204804) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
